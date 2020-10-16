a3fb5fa07a60bd3923f60b29860ad1a176d7ffff
EmiPeralta <emilianop96@hotmail.com>
2020-10-16 03:21:00 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,79 @@
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    };
+};
+
+class UI {
+    static displayBooks() {
+        const StoredBooks = [
+            {
+                title: 'Book one',
+                author: 'Joan Herrera',
+                isbn: '3434434'
+            },
+            {
+                title: 'Book two',
+                author: 'Emiliano Peralta',
+                isbn: '4343343'
+            }
+        ];
+
+        const books = StoredBooks;
+
+        books.forEach(book => UI.addBookToList(book));
+    };
+
+    static addBookToList(book) {
+        const list = document.querySelector('#book-list');
+
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td><a href='#' class='btn btn-danger delete'>X</a></td>
+        `;
+
+        list.appendChild(row);
+    };
+
+    static showAlert(message, className) {
+        const div = document.createElement('div');
+        div.className = `alert ${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+    };
+
+    static deleteBook(el) {
+        if (el.classList.contains('delete')) {
+            el.parentElement.parentElement.remove();
+        };
+    };
+};
+
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    if (title === '' || author === '' || isbn === '') UI.showAlert('Please fill in all fields', 'danger');
+    else {
+        const book = new Book(title, author, isbn);
+        UI.addBookToList(book);
+        document.getElementById('book-form').reset();
+    }
+});
+
+document.querySelector('#book-list').addEventListener('click', e => {
+    UI.deleteBook(e.target);
+});
